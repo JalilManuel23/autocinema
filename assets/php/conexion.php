c34850b6d4fff35fa8c5cd670d95ef1d850e5182
jalilmanuel25 <jalilmanuel25@gmail.com>
2022-07-14 21:24:55 -0500

    Inicio repositorio CRM

---
+++ assets/php/conexion.php
@@ -0,0 +1,16 @@
+<?php
+
+$host_db = "localhost";
+$user_db = "root";
+$pass_db = "";
+$db_name = "cartelera";
+
+
+$conn = new mysqli($host_db, $user_db, $pass_db, $db_name);
+if (!$conn->set_charset("utf8")) {
+    //printf("Error cargando el conjunto de caracteres utf8: %s\n", $conn->error);
+    exit();
+} else {
+    //printf("Conjunto de caracteres actual: %s\n", $conn->character_set_name());
+}
+?>
