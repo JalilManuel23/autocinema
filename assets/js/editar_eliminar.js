584d94b8eed159a18f2c85231195d15acc2b8b54
jalilmanuel25 <jalilmanuel25@gmail.com>
2022-08-11 22:14:13 -0500

    Eliminar código no necesario

--- assets/js/editar_eliminar.js
+++ assets/js/editar_eliminar.js
@@ -1,21 +1,3 @@
-// $(document).on("change", "#img_editar", function () {
-//   let imgCodificada = URL.createObjectURL(this.files[0]);
-//   $("#imgPrev_editar").attr("src", imgCodificada);
-
-//   let archivoInput = document.getElementById("img_editar").value;
-//   console.log(archivoInput);
-//   let extPermitidas =
-//     /(.gif|.svg|.png|.jpeg|.jpg|.webp|.GIF|.SVG|.PNG|.JPEG|.JPG|.WEBP)$/i;
-//   if (!extPermitidas.exec(archivoInput)) {
-//     document.getElementById("img_editar").value = null;
-//     Swal.fire(
-//       "Aviso!",
-//       "Extensión no valida, asegurate de haber seleccionado una imagen",
-//       "warning"
-//     );
-//   }
-// });
-
 if (document.getElementById("formEditar")) {
   document.getElementById("btnEditar").addEventListener("click", (e) => {
     e.preventDefault();
