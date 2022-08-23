a4d0920f9f0a0703965645e26f0eac30168054f8
Emmanuel  Gutierrez <manuel15901@gmail.com>
2022-08-23 00:02:37 -0500

    Estilos Beto

--- php/clientes/modificar.php
+++ php/clientes/modificar.php
@@ -52,43 +52,92 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="../../assets/img/favicon.png">
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+
+    <link href="../../assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="../../assets/css/nucleo-svg.css" rel="stylesheet" />
+
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link href="../../assets/css/nucleo-svg.css" rel="stylesheet" />
+
+    <link id="pagestyle" href="../../assets/css/soft-ui-dashboard.css?v=1.0.6" rel="stylesheet" />
+
+
     <title>Edita tu perfil</title>
 </head>
 
 <body>
     <div>
         <center>
-            <h1>Actualizar tu Información</h1>
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
-                <label for="nombre">Nombre:</label>
-                <input type="text" name="nombre" placeholder="Edita tu nombre" value="<?php echo $fila['nombre']; ?>" required>
-                <label for="ape_paterno"></label>
-                <input type="text" name="ape_paterno" placeholder="Edita tu Apellido Paterno" value="<?php echo $fila['ape_paterno']; ?>">
-                <label for="ape_materno"></label>
-                <input type="text" name="ape_materno" placeholder="Edita tu Apellido Materno" value="<?php echo $fila['ape_materno']; ?>">
-                <label for="bio"></label>
-                <input type="text" name="bio" placeholder="Edita tu Biografia" value="<?php echo $fila['bio']; ?>">
-                <label for="calle"></label>
-                <input type="text" name="calle" placeholder="Edita tu Calle" value="<?php echo $fila['calle']; ?>">
-                <label for="col"></label>
-                <input type="text" name="col" placeholder="Edita tu Colonia" value="<?php echo $fila['col']; ?>">
-                <label for="numero"></label>
-                <input type="text" name="numero" placeholder="Edita tu Numero de vivienda" value="<?php echo $fila['numero']; ?>">
-                <label for="zipcode"></label>
-                <input type="text" name="zipcode" placeholder="Edita tu Codigo Postal" value="<?php echo $fila['zipcode']; ?>">
-                <label for="email">Correo:</label>
-                <input type="email" name="email" placeholder="Edita tu Correo Electronico" value="<?php echo $fila['email']; ?>" required>
-                <label for="pass">Contraseña:</label>
-                <input type="password" name="pass" placeholder="Nueva contraseña" value="<?php echo $fila['pass']; ?>" required>
-                <label for="telefono">Numero de Telefono:</label>
-                <input type="number" name="telefono" placeholder="Nueva Numero de telefono" value="<?php echo $fila['telefono']; ?>" required>
-                <br><br>
-                <input type="hidden" name="id" value="<?php echo $fila['id']; ?>">
-                <input class="btn" type="submit" value="Actualizar" name="envio">
-            </form>
+            <h2 class="font-weight-bolder text-info text-gradient">Actualiza tu Información</h2>
+            <main class="main-content mt-0">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
+                            <div class="card card-plain mt-8">
+                                <div class="card-header pb-0 text-left bg-transparent">
+                                    <div class="card-body">
+                                        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+                                            <div class="mb-3">
+                                                <label for="nombre">Nombre: </label>
+                                                <input class="mb-3" type="text" name="nombre" placeholder="Edita tu nombre" value="<?php echo $fila['nombre']; ?>" required>
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="ape_paterno">Apellido Paterno: </label>
+                                                <input class="mb-3" type="text" name="ape_paterno" placeholder="Edita tu Apellido Paterno" value="<?php echo $fila['ape_paterno']; ?>">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="ape_materno">Apellido Materno: </label>
+                                                <input class="mb-3" type="text" name="ape_materno" placeholder="Edita tu Apellido Materno" value="<?php echo $fila['ape_materno']; ?>">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="bio">Biografia: </label>
+                                                <input type="text" name="bio" placeholder="Edita tu Biografia" value="<?php echo $fila['bio']; ?>">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="calle">Tu Calle: </label>
+                                                <input type="text" name="calle" placeholder="Edita tu Calle" value="<?php echo $fila['calle']; ?>">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="col">Colonia: </label>
+                                                <input type="text" name="col" placeholder="Edita tu Colonia" value="<?php echo $fila['col']; ?>">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="numero">Numero de Vivienda: </label>
+                                                <input type="text" name="numero" placeholder="Edita tu Numero de vivienda" value="<?php echo $fila['numero']; ?>">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="zipcode">Codigo Postal: </label>
+                                                <input type="text" name="zipcode" placeholder="Edita tu Codigo Postal" value="<?php echo $fila['zipcode']; ?>">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="email">Correo: </label>
+                                                <input type="email" name="email" placeholder="Edita tu Correo Electronico" value="<?php echo $fila['email']; ?>" required>
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="pass">Contraseña: </label>
+                                                <input type="password" name="pass" placeholder="Nueva contraseña" value="<?php echo $fila['pass']; ?>" required>
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="telefono">Numero de Telefono: </label>
+                                                <input type="number" name="telefono" placeholder="Nueva Numero de telefono" value="<?php echo $fila['telefono']; ?>" required>
+                                            </div>
+                                            <br><br>
+                                            <input type="hidden" name="id" value="<?php echo $fila['id']; ?>">
+                                            <input class="btn" type="submit" value="Actualizar" name="envio">
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </center>
         <a class="btn" href="../../pages/cliente.php">Volver</a>
-        <a class="btn" href="eliminar.php?id=<?php echo $fila['id'];?>">ELIMINAR CUENTA</a>
+        <a style="color: red" class="btn" href="eliminar.php?id=<?php echo $fila['id']; ?>">ELIMINAR CUENTA</a>
     </div>
 </body>
 
