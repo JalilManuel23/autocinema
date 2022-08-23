4ee9fb7b6601302702045b0d2547fabf9f00362e
JaquelineMedina <jaquelinegpem@gmail.com>
2022-08-23 11:42:40 -0500

    observaciones

--- pages/observaciones.php
+++ pages/observaciones.php
@@ -44,7 +44,7 @@ include("./partials/scripts.php");
                         <td>
                           <div class="d-flex px-2 py-1">
                             <div>
-                              <img src="../assets/img/" class="avatar avatar-sm me-3" alt="user1">
+                              <img src="../assets/img/ja.jpeg" class="avatar avatar-sm me-3" alt="user1">
                             </div>
                             <div class="d-flex flex-column justify-content-center">
                               <h6 class="mb-0 text-sm">Jaqueline Medina</h6>
@@ -71,158 +71,10 @@ include("./partials/scripts.php");
                       </tr>
                       <tr>
                         <td>
-                          <div class="d-flex px-2 py-1">
-                            <div>
-                              <img src="../assets/img/team-3.jpg" class="avatar avatar-sm me-3" alt="user2">
-                            </div>
-                            <div class="d-flex flex-column justify-content-center">
-                              <h6 class="mb-0 text-sm">Alexa Liras</h6>
-                              <p class="text-xs text-secondary mb-0">alexa@gmail.com</p>
-                            </div>
-                          </div>
-                        </td>
-                        <td>
-                          <p class="text-xs text-secondary mb-0">Cliente</p>
-                        </td>
-                        <td class="align-middle text-center text-sm">
-                          <span class="badge badge-sm bg-gradient-secondary">Desconectado</span>
-                        </td>
-                        <td class="align-middle text-center">
-                          <span class="text-secondary text-xs font-weight-bold">11/01/19</span>
-                        </td>
-                        <td class="align-middle">
-                          <a href="respuestas.php" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip"
-                            data-original-title="Edit user">
-                            Ver
-                          </a>
-                        </td>
-                        
-                      </tr>
-                      <tr>
-                        <td>
-                          <div class="d-flex px-2 py-1">
-                            <div>
-                              <img src="../assets/img/team-4.jpg" class="avatar avatar-sm me-3" alt="user3">
-                            </div>
-                            <div class="d-flex flex-column justify-content-center">
-                              <h6 class="mb-0 text-sm">Laurent Perrier</h6>
-                              <p class="text-xs text-secondary mb-0">laurent@gmail.com</p>
-                            </div>
-                          </div>
-                        </td>
-                        <td>
-                          <p class="text-xs text-secondary mb-0">Cliente</p>
-                        </td>
-                        <td class="align-middle text-center text-sm">
-                          <span class="badge badge-sm bg-gradient-success">Conectado</span>
-                        </td>
-                        <td class="align-middle text-center">
-                          <span class="text-secondary text-xs font-weight-bold">19/09/17</span>
-                        </td>
-                        <td class="align-middle">
-                          <a href="respuestas.php" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip"
-                            data-original-title="Edit user">
-                            Ver
-                          </a>
-                        </td>
-                       
-                      </tr>
-                      <tr>
-                        <td>
-                          <div class="d-flex px-2 py-1">
-                            <div>
-                              <img src="../assets/img/team-3.jpg" class="avatar avatar-sm me-3" alt="user4">
-                            </div>
-                            <div class="d-flex flex-column justify-content-center">
-                              <h6 class="mb-0 text-sm">Michael Levi</h6>
-                              <p class="text-xs text-secondary mb-0">michael@gmail.com</p>
-                            </div>
-                          </div>
-                        </td>
-                        <td>
-                          <p class="text-xs text-secondary mb-0">Cliente</p>
-                        </td>
-                        <td class="align-middle text-center text-sm">
-                          <span class="badge badge-sm bg-gradient-success">Conectado</span>
-                        </td>
-                        <td class="align-middle text-center">
-                          <span class="text-secondary text-xs font-weight-bold">24/12/08</span>
-                        </td>
-                        <td class="align-middle">
-                          <a href="respuestas.php" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip"
-                            data-original-title="Edit user">
-                            Ver
-                          </a>
-                        </td>
-                       
-                      </tr>
-                      <tr>
-                        <td>
-                          <div class="d-flex px-2 py-1">
-                            <div>
-                              <img src="../assets/img/team-2.jpg" class="avatar avatar-sm me-3" alt="user5">
-                            </div>
-                            <div class="d-flex flex-column justify-content-center">
-                              <h6 class="mb-0 text-sm">Richard Gran</h6>
-                              <p class="text-xs text-secondary mb-0">richard@gmail.com</p>
-                            </div>
-                          </div>
-                        </td>
-                        <td>
-                          <p class="text-xs text-secondary mb-0">Cliente</p>
-                        </td>
-                        <td class="align-middle text-center text-sm">
-                          <span class="badge badge-sm bg-gradient-secondary">Desconectado</span>
-                        </td>
-                        <td class="align-middle text-center">
-                          <span class="text-secondary text-xs font-weight-bold">04/10/21</span>
-                        </td>
-                        <td class="align-middle">
-                          <a href="respuestas.php" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip"
-                            data-original-title="Edit user">
-                            Ver
-                          </a>
-                        </td>
-                        
-                      </tr>
-                      <tr>
-                        <td>
-                          <div class="d-flex px-2 py-1">
-                            <div>
-                              <img src="../assets/img/team-4.jpg" class="avatar avatar-sm me-3" alt="user6">
-                            </div>
-                            <div class="d-flex flex-column justify-content-center">
-                              <h6 class="mb-0 text-sm">Miriam Eric</h6>
-                              <p class="text-xs text-secondary mb-0">miriam@gmail.com</p>
-                            </div>
-                          </div>
-                        </td>
-                        <td>
-                          <p class="text-xs text-secondary mb-0">Cliente</p>
-                        </td>
-                        <td class="align-middle text-center text-sm">
-                          <span class="badge badge-sm bg-gradient-secondary">Desconectado</span>
-                        </td>
-                        <td class="align-middle text-center">
-                          <span class="text-secondary text-xs font-weight-bold">14/09/20</span>
-                        </td>
-                        <td class="align-middle">
-                          <a href="respuestas.php" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip"
-                            data-original-title="Edit user">
-                            Ver
-                          </a>
-                        </td>
-                        
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </div>
-          </div>
+                         
         </div>
+        
         <?php
-        include './partials/footer.php';
         ?>
       </div>
     </div>
--- pages/respuestas.php
+++ pages/respuestas.php
@@ -18,13 +18,47 @@ include("./partials/navbarvertical.php");
   <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
     <?php
     include("./partials/navbarhorizontal.php");
-    ?>
-    <div class="card-body pt-4 p-3">
+    $result = $conn->query("SELECT * FROM comentarios");
+    // mysqli_free_result($result)
+    while ($row = $result->fetch_assoc()) {
+      ?>
+       <div class="card-body pt-4 p-3">
               <ul class="list-group">
                 <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg">
                   <div class="d-flex flex-column">
-                    <h6 class="mb-3 text-sm">Jaqueline Medina</h6>
-                    <span class="mb-2 text-xs">Es muy facil e intuitivo de utilizar, ademas de que aqui estan mis peliculas favoritas <span class="text-dark font-weight-bold ms-sm-2"></span></span>
+                    <h6 class="mb-3 text-sm">Jaqueline Medina </h6>
+                    <span class="mb-2 text-xs"><?php
+                    
+                    printf ($row['comentarios']);
+                    if ($row['respuesta']){
+                      ?>
+                      <div class="card-body pt-4 p-3">
+                      <ul class="list-group">
+                        <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg">
+                          <div class="d-flex flex-column">
+                            <h6 class="mb-3 text-sm">Autocinema </h6>
+                            <span class="mb-2 text-xs">
+
+                            <?php
+                      printf('Gracias por tu comentario. Si tienes alguna queja, puedes comunicarte al numero 55478654');
+                      ?>
+
+                      </span>
+
+                      </div>
+</li>
+</ul>
+</div>
+<?php
+
+
+                      
+
+                    }
+
+
+?>
+<span class="text-dark font-weight-bold ms-sm-2"></span></span>
                     <HEAD> <script LANGUAGE="JavaScript">
 function agregar(){
 if ((navigator.appName=="Microsoft Internet Explorer") && (parseInt(navigator.appVersion)>=4)) {
@@ -46,10 +80,7 @@ swal ("¿Seguro que desea agregar a favoritos?", {
 
                   </div>
                   <div class="ms-auto text-end">
-                    <a class="btn btn-link text-dark px-3 mb-0" href="javascript:;"><i class="fas fa-pencil-alt text-dark me-2" aria-hidden="true"></i>Editar</a>
-                    <input type="text" name="responder">
-
-                    <button onclick="responder()">Responder</button>
+                   
                     <script type="text/javascript">
                       function responder() {
                         swal ('Respuesta Enviada', 'Respuesta enviada al cliente', 'success');
@@ -75,134 +106,20 @@ swal ("¿Seguro que desea agregar a favoritos?", {
                 </li>
                
             </div>
+            <?php
 
-            <div class="card-body pt-4 p-3">
-              <ul class="list-group">
-                <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg">
-                  <div class="d-flex flex-column">
-                    <h6 class="mb-3 text-sm">Alexa Liras</h6>
-                    <span class="mb-2 text-xs">Que buen servicio la verdad, desde comprar los boletos. <span class="text-dark font-weight-bold ms-sm-2"></span></span>
-                    <HEAD> <script LANGUAGE="JavaScript">
-function agregar(){
-if ((navigator.appName=="Microsoft Internet Explorer") && (parseInt(navigator.appVersion)>=4)) {
-var url="http://www.TuSitio.com/";
-var titulo=" Descripcion de mi sitio web";
-window.external.AddFavorite(url,titulo);
-}
-else {
-if(navigator.appName == "Netscape")
-swal ("¿Seguro que desea agregar a favoritos?", {
-  buttons: ["Si", "No"]
-});
-}
-}
-</script>
-
-</HEAD> <BODY>
-<img src="../assets/img/thumbs-up.png" class="avatar avatar-sm me-3" alt="user1" onclick="agregar()" href="javascript:agregar()">
-                  </div>
-                  <div class="ms-auto text-end">
-                    <a class="btn btn-link text-dark px-3 mb-0" href="javascript:;"><i class="fas fa-pencil-alt text-dark me-2" aria-hidden="true"></i>Editar</a>
-                    <input type="text" name="responder">                    
-                    <button onclick="responder()">Responder</button>
-                    <script type="text/javascript">
-                      function responder() {
-                        swal ('Respuesta Enviada', 'Respuesta enviada al cliente', 'success');
-
-                      }
-
-                    </script>
-
-                  </div>
-                </li>
-               
-            </div>
-
-            <div class="card-body pt-4 p-3">
-              <ul class="list-group">
-                <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg">
-                  <div class="d-flex flex-column">
-                    <h6 class="mb-3 text-sm">Laurent Perrier</h6>
-                    <span class="mb-2 text-xs">Me gustaria saber, ¿Cada cuanto hacen cambios en las peliculas?.<span class="text-dark font-weight-bold ms-sm-2"></span></span>
-                    <HEAD> <script LANGUAGE="JavaScript">
-function agregar(){
-if ((navigator.appName=="Microsoft Internet Explorer") && (parseInt(navigator.appVersion)>=4)) {
-var url="http://www.TuSitio.com/";
-var titulo=" Descripcion de mi sitio web";
-window.external.AddFavorite(url,titulo);
-}
-else {
-if(navigator.appName == "Netscape")
-swal ("¿Seguro que desea agregar a favoritos?", {
-  buttons: ["Si", "No"]
-});
-}
-}
-</script>
-
-</HEAD> <BODY>
-<img src="../assets/img/thumbs-up.png" class="avatar avatar-sm me-3" alt="user1" onclick="agregar()" href="javascript:agregar()">
-                  </div>
-                  <div class="ms-auto text-end">
-                    <a class="btn btn-link text-dark px-3 mb-0" href="javascript:;"><i class="fas fa-pencil-alt text-dark me-2" aria-hidden="true"></i>Editar</a>
-                    <input type="text" name="responder">
-                    <button onclick="responder()">Responder</button>
-                    <script type="text/javascript">
-                      function responder() {
-                        swal ('Respuesta Enviada', 'Respuesta enviada al cliente', 'success');
-
-                      }
-
-                    </script>
+  }
+  ?>
 
 
-                  </div>
-                </li>
-               
-            </div>
 
-            <div class="card-body pt-4 p-3">
-              <ul class="list-group">
-                <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg">
-                  <div class="d-flex flex-column">
-                    <h6 class="mb-3 text-sm">Michael Levi</h6>
-                    <span class="mb-2 text-xs">La verdad, tuve una excelente experiencia buscando mis peliculas favoritas<span class="text-dark font-weight-bold ms-sm-2"></span></span>
-                    <HEAD> <script LANGUAGE="JavaScript">
-function agregar(){
-if ((navigator.appName=="Microsoft Internet Explorer") && (parseInt(navigator.appVersion)>=4)) {
-var url="http://www.TuSitio.com/";
-var titulo=" Descripcion de mi sitio web";
-window.external.AddFavorite(url,titulo);
-}
-else {
-if(navigator.appName == "Netscape")
-swal ("¿Seguro que desea agregar a favoritos?", {
-  buttons: ["Si", "No"]
-});
-}
-}
-</script>
+    
+    
+    
 
 </HEAD> <BODY>
 
-<img src="../assets/img/thumbs-up.png" class="avatar avatar-sm me-3" alt="user1" onclick="agregar()" href="javascript:agregar()">
-                  </div>
-                  <div class="ms-auto text-end">
-                    <a class="btn btn-link text-dark px-3 mb-0" href="javascript:;"><i class="fas fa-pencil-alt text-dark me-2" aria-hidden="true"></i>Editar</a>
-                    <input type="text" name="responder">            
-                    <button onclick="responder()">Responder</button>
-                    <script type="text/javascript">
-                      function responder() {
-                        swal ('Respuesta Enviada', 'Respuesta enviada al cliente', 'success');
-
-                      }
-
-                    </script>
-      
-                  </div>
-                </li>
-               
-            </div>
+                 
  </main>
   <?php
   include './partials/personalizacion.php';
