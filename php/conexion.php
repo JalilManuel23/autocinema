a7e7fcedb84509041890a6f4c717aa64ee46c511
Gerardo Ceseñas Rivera <67026528+GCesenas@users.noreply.github.com>
2022-08-24 08:48:24 -0500

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -5,12 +5,6 @@
 // $pass_db = "";
 // $db_name = "autocinema";
 
-$host_db = "localhost:3306";
-$user_db = "cesenasc_user";
-$pass_db = "aHx9Mz1XZoro";
-$db_name = "cesenasc_autocinema";
-
-
 $conn = new mysqli($host_db, $user_db, $pass_db, $db_name);
 if (!$conn) {
     // printf("Error cargando el conjunto de caracteres utf8: %s\n", $conn->error);
